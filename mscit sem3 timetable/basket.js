927d8f7e380f8be44cb08242611f37fa87d9069b
Hii <43162530+sun-m00n@users.noreply.github.com>
2022-08-30 13:48:55 +0530

    Add files via upload

--- mscit sem3 timetable/basket.js
+++ mscit sem3 timetable/basket.js
@@ -7,18 +7,28 @@ function Tag(arg = { tag: 'p', attr: {}, content: '' }) {
 }
 
 let config = {
-    refetch: false
+    update: false
 }
 
 // export Basket variable
 export const Basket = {
-    // config: function (arg = { refetch: false }) {
-    //     config.refetch = arg.refetch
-    // },
+    config: function (arg = { updated_on: undefined }) {
+        let updated = parseInt(localStorage.getItem('updated_on'))
+        if (updated && updated != arg.updated_on) {
+            config.update = true
+            for (let key in arg) {
+                config[key] = arg[key]
+                localStorage.setItem(key, arg[key])
+            }
+        } else if (updated == null) {
+            localStorage.setItem('updated_on', arg.updated_on)
+            config.update = true
+        }
+    },
     require: function (...args) {
         for (let arg of args) {
             let key = (arg.key != null) ? arg.key : arg.url;
-            let should_refetch = (arg.refetch) ? true : (Basket.load(key)) ? false : true;
+            let should_refetch = (config.update) ? true : (Basket.load(key)) ? false : true;
             if (should_refetch) {
                 fetch(arg.url, { method: 'GET', redirect: 'follow' })
                     .then(response => response.text())
@@ -32,10 +42,8 @@ export const Basket = {
     load: function (key) {
         if (localStorage.getItem(key) != null) {
             let file_type = ((key.split('.')).slice(-1))[0]
-            if (file_type === 'js')
-                document.querySelector('head').append(Tag({ tag: 'script', attr: { defer: true }, content: localStorage.getItem(key) }))
-            else if (file_type === 'css')
-                document.querySelector('head').append(Tag({ tag: 'style', content: localStorage.getItem(key) }))
+            if (file_type === 'js') document.querySelector('head').append(Tag({ tag: 'script', attr: { defer: true }, content: localStorage.getItem(key) }))
+            else if (file_type === 'css') document.querySelector('head').append(Tag({ tag: 'style', content: localStorage.getItem(key) }))
             return true
         }
         return false
