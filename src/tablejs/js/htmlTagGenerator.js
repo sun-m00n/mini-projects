d7e23c7999abd6bb7412816826339c9920201917
sun-m00n <infinityknight024@gmail.com>
2023-01-13 01:54:06 +0530

    uwfbviweuv

--- src/tablejs/js/htmlTagGenerator.js
+++ src/tablejs/js/htmlTagGenerator.js
@@ -1,12 +1,132 @@
+// function $t(tagStr, childrens = []) {
+//     let htmlTag;
 
-function _TAG_(array) {
+//     let str = tagStr.split(/[@]/gm)
+//     let attrs = (str[1])?.split(',') || []
+
+//     htmlTag = document.createElement(str[0]) // str[0] = tag-name
+
+//     for (let attr of attrs) {
+//         attr = attr.split('=')
+//         htmlTag.setAttribute(attr[0], attr[1])
+//     }
+
+//     if (childrens.length > 0 && Array.isArray(childrens))
+//         htmlTag.append(...childrens)
+
+//     return htmlTag;
+// }
+function $tv2(tagStr, childrens = []) {
     let htmlTag;
 
-    for (let parent of array) {
+    // console.log(tagStr)
+    // if (Array.isArray(tagStr)) {
+    //     tagStr = tagStr[0]
+    //     childrens = tagStr[1]
+    // }
 
+    let str = tagStr.split(/[@]/gm)
+    let attrs = (str[1])?.split(',') || []
+
+    htmlTag = document.createElement(str[0]) // str[0] = tag-name
+
+    for (let attr of attrs) {
+        attr = attr.split('=')
+        htmlTag.setAttribute(attr[0], attr[1])
     }
-    let parent = Object.keys(array)[0]
 
+    if (childrens.length > 0 && Array.isArray(childrens)) {
+        childrens.forEach(child => {
+            let tagName = (typeof child === 'string') ? child : child[0],
+                tagChildrens = (typeof child === 'object') ? child[1] : [];
+            // tagChildrens = (typeof child[1] === 'string') ? child[1] : []
+            htmlTag.append($tv2(tagName, tagChildrens))
+        })
+    } else if (typeof childrens === 'string') {
+        htmlTag.append(childrens)
+    }
 
     return htmlTag;
 }
+// ("table",
+//     [
+//         ["thead",
+//             ['tr',
+//                 [
+//                     ['th', 'siru'],
+//                     ['th', 's2']
+//                 ]
+//             ]
+//         ],
+//         ['tbody']
+//     ]
+// )
+let param = ['table', [
+    ['thead', [
+        ['tr', [
+            ['th', 'a'],
+            ['th', 'b'],
+            ['th', 'c']]
+        ]]],
+    ['tbody']
+]]
+
+function $tv3(array) {
+    let htmlTag;
+
+    let str = array[0].split(/[@]/gm)
+    let attrs = (str[1])?.split(',') || []
+
+    htmlTag = document.createElement(str[0]) // str[0] = tag-name
+
+    for (let attr of attrs) {
+        attr = attr.split('=')
+        htmlTag.setAttribute(attr[0], attr[1])
+    }
+
+    if (array[1] && array[1].length > 0 && Array.isArray(array[1])) {
+        array[1].forEach(arr => htmlTag.append($tv3(arr)))
+    }
+    else if (typeof array[1] === 'string') {
+        htmlTag.append(array[1])
+    }
+
+    return htmlTag;
+}
+
+
+// function _TAG_(object) {
+//     let htmlTag;
+//     for (let parent in object) {
+//         let str = parent.split(/[@]/gm)
+//         // let str = parent.split(/[@&]/gm)
+//         let attrs = (str[1])?.split(',') || []
+//         // let events = (str[2]).split(',') || []
+
+//         htmlTag = document.createElement(str[0]) // str[0] = tag-name
+
+//         for (let attr of attrs) {
+//             attr = attr.split('=')
+//             htmlTag.setAttribute(attr[0], attr[1])
+//         }
+
+//         if (typeof object[parent] === 'object') {
+//             htmlTag.append(_TAG_(object[parent]))
+//         }
+
+//         // if (typeof object[parent] != 'object') {
+//         if (object[parent].length > 0 || typeof object[parent] === 'string')
+//             htmlTag.append(document.createTextNode(object[parent]))
+//         // }
+//     }
+//     return htmlTag;
+// }
+
+/**
+ * @notes
+ * var str="ao3 ako lopo"
+ * var index = str.indexOf(" ") // get first index of " "
+ * var firstStr=str.subString(0,index)
+ * var remainingStr=str.subString(index+1)
+ */
+// _TAG_({ "div@id=box,class=card,data-id=45": { "div@id=box1,class=card,data-id=44": { "p": "siro" } } })
--- src/tablejs/js/index.js
+++ src/tablejs/js/index.js
@@ -10,7 +10,8 @@ class ExcelTable {
         pagination_align: 'bottom-right', // top-left | top-center | top-right | bottom-left | bottom-center | bottom-right
         max_rows_per_page: 100, //  miniumum: 0 | maximum: 100 (default)
         search: true, // true | false
-        search_align: 'top-left' // top-left | top-center | top-right | bottom-left | bottom-center | bottom-right
+        search_align: 'top-left', // top-left | top-center | top-right | bottom-left | bottom-center | bottom-right
+        tableId: ''
     }
     // helper function
     constructor(headers, options = this.options) {
@@ -24,13 +25,49 @@ class ExcelTable {
         delete options.containerId
         this.options = { ...this.options, ...options }
 
-        // add table
+        // generate header
+        let ths = []
+        this.headers.forEach(function (th) {
+            ths.push(['th', th])
+        })
 
+        // generate table
+        // let table = _TAG_({ "table": { "thead": { 'tr': '' }, "tbody": '' } })
+        // let table = $tv3("table", [["thead", ['tr', [['th', 'siru'], ['th', 's2']]]], ['tbody']])
+        // let table = $tv3(["table", [["thead", ['tr', [['th', 'siru'], ['th', 's2']]]], ['tbody']]])
+        let param = [`table${(this.options.tableId) ? '@' + this.options.tableId : ''}`, [
+            ['thead', [
+                ['tr',
+                    // [['th', 'a'],
+                    // ['th', 'b'],
+                    // ['th', 'c']]
+                    ths
+                ]]],
+            ['tbody']
+        ]]
+
+        let table = $tv3(param)
+        // add headers
+        // table.querySelector('thead tr').append(...ths)
+        // add table
+        this._('#' + this.containerId).append(table)
     }
 
-    add(data) {
-
-
+    add(datas) {
+        let tbody = this._('#' + this.containerId + ' tbody')
+        datas.forEach(data => {
+            let tds = new Array(this.headers.length)
+            for (let key in data) {
+                let column_index = this.headers.indexOf(key)
+                tds[column_index] = $tv3(['td', data[key]])
+            }
+            let tr = $tv3(['tr'])
+            for (let i = 0; i < tds.length; i++) {
+                tds[i] = (typeof tds[i] === 'undefined') ? $tv3(['tr']) : tds[i]
+            }
+            tr.append(...tds)
+            tbody.append(tr)
+        })
     }
 
 
